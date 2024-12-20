d8d7fdd6c82092e53629f888483120fda926fb5f
shafira az-zahra <shafiraa66@gmail.com>
2024-12-20 11:00:42 +0700

    Tugas

---
+++ Tugaspbw-sticky/sticky22-25/app/Providers/AppServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+use App\Models\Store;
+use App\Policies\StorePolicy;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        Gate::policy(Store::class, StorePolicy::class);
+    }
+}
